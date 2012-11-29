5780c459450bd6d0acd5271e5b15e08bc61283ee
Drarok Ithaqua <github@catchall.drarok.com>
2012-11-29 15:52:16 +0000

    Make the ImageJet core class much smarter, so using it is a lot easier.

--- classes/imagejet/imagejet.php
+++ classes/imagejet/imagejet.php
@@ -1,61 +1,139 @@
 <?php
 
 class ImageJet_ImageJet {
-	protected $path;
+	/**
+	 * Name of the config group to use for generated thumbnails.
+	 *
+	 * @var string
+	 */
+	protected $_group;
 
-	public function __construct($path) {
-		$this->path = $path;
+	/**
+	 * Config cache.
+	 *
+	 * @var array
+	 */
+	protected $_config;
+
+	/**
+	 * Image root cache.
+	 *
+	 * @var string
+	 */
+	protected $_image_root;
+
+	/**
+	 * Object constructor.
+	 *
+	 * @param string $group Config group to use for generated thumbnails.
+	 */
+	public function __construct($group) {
+		$this->_group = $group;
 	}
 
-	public function thumbnail($config, $new_path) {
-		// If the config is a string, load the array.
-		if (is_string($config)) {
-			$config = Kohana::$config->load('imagefly.' . $config);
-		}
+	/**
+	 * Create a thumbnail from a given image path.
+	 *
+	 * @param string $image_path Path to the original image, relative to configured image-root.
+	 * @param bool   $output     Pass true to output the generated thumbnail.
+	 *
+	 * @return void
+	 */
+	public function thumbnail($image_path, $output = false) {
+		// You can't trust anybody these days.
+		$image_path = str_replace('..', '', $image_path);
 
-		// If it's not an array, then bail.
-		if (! is_array($config)) {
-			throw new Exception('Invalid image config');
-		}
+		// Grab the config.
+		$config = $this->_get_config();
+
+		// Build the paths to the files.
+		$original_path = $this->_get_image_root() . $image_path;
+		$thumb_path = $this->_get_image_root() . $this->_group . DIRECTORY_SEPARATOR . $image_path;
 
 		// Ensure the path exists.
-		$dir = dirname($new_path);
+		$dir = dirname($thumb_path);
 		if (! is_dir($dir)) {
-			mkdir($dir, 0775, TRUE);
+			if (! mkdir($dir, Kohana::$config->load('imagejet.folder-permissions'), TRUE)) {
+				throw new Exception('Failed to create directory.');
+			}
 		}
 
-		// Resize and save the image.
-		$image = Image::factory($this->path);
-		$image->resize($config['width'], $config['height']);
-		$image->save($new_path);
+		// Grab repeated config parameters.
+		$width = $config['width'];
+		$height = $config['height'];
+
+		// Resize and save to the thumbnail file.
+		$image = Image::factory($original_path);
+		$image->resize($width, $height);
+		$image->save($thumb_path);
 		unset($image);
 
 		// Create the new image resource.
-		$image = imagecreatetruecolor($w = $config['width'], $h = $config['height']);
+		$image = imagecreatetruecolor($width, $height);
 
 		// Fill with the background colour.
 		list($r, $g, $b) = $config['background'];
 		$background = imagecolorallocate($image, $r, $g, $b);
 		imagefill($image, 0, 0, $background);
 
-		// Bring in the original image.
-		$orig = imagecreatefromjpeg($new_path);
-		$sx = imagesx($orig); $sy = imagesy($orig);
-		imagecopy($image, $orig, ($w - $sx) / 2, ($h - $sy) / 2, 0, 0, $sx, $sy);
+		// Bring in the thumbnail image, centred in the background.
+		$orig = imagecreatefromjpeg($thumb_path);
+		$sx = imagesx($orig);
+		$sy = imagesy($orig);
+		imagecopy($image, $orig, ($width - $sx) / 2, ($height - $sy) / 2, 0, 0, $sx, $sy);
 
-		// Add the border.
+		// Add the border *after* we import the thumbnail, if enabled.
 		if ((bool) $config['border']) {
 			list ($r, $g, $b) = $config['border'];
 			$border = imagecolorallocate($image, $r, $g, $b);
-			imagerectangle($image, 0, 0, $w - 1, $h - 1, $border);
+			imagerectangle($image, 0, 0, $width - 1, $height - 1, $border);
 		}
 
 		// Re-save our processed image.
-		imagejpeg($image, $new_path);
+		imagejpeg($image, $thumb_path);
 
-		// Output the file.
-		header('Content-Type: image/jpeg');
-		$file = fopen($new_path, 'r');
-		fpassthru($file);
+		// Output the file?
+		if ($output) {
+			header('Content-Type: image/jpeg');
+			$file = fopen($thumb_path, 'r');
+			fpassthru($file);
+		}
+	}
+
+	/**
+	 * Cached getter for the config.
+	 *
+	 * @return array
+	 */
+	protected function _get_config() {
+		if ($this->_config === NULL) {
+			$this->_config = Kohana::$config->load('imagejet.' . $this->_group);
+
+			if (! $this->_config) {
+				throw new Exception('No such group configured: ' . $this->_group);
+			}
+		}
+
+		return $this->_config;
+	}
+
+	/**
+	 * Cached getter for the image root.
+	 *
+	 * @return string
+	 */
+	protected function _get_image_root() {
+		if ($this->_image_root === NULL) {
+			$this->_image_root = Kohana::$config->load('imagejet.image-root');
+
+			if (! $this->_image_root) {
+				throw new Exception('Invalid image root configuration.');
+			}
+
+			// Make sure we have a slash on the end, but not two.
+			$this->_image_root = rtrim($this->_image_root, '\/') . DIRECTORY_SEPARATOR;
+		}
+
+		return $this->_image_root;
 	}
 }
